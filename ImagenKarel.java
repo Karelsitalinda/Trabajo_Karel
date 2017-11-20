4e682a24f4050c94807e9cc2292f0c794df044d5
julianduar <33558010+julianduar@users.noreply.github.com>
2017-11-20 17:46:15 -0500

    corrección imagen y movimiento en x e y
    Se modificó la dirección de la imagen para que se pueda trabajar en cualquier computador, además se crearon métodos para el movimiento en x e y

--- ImagenKarel.java
+++ ImagenKarel.java
@@ -3,14 +3,31 @@ import acm.program.GraphicsProgram;
 import acm.graphics.*;
 
 
-public class imagenKarel extends GraphicsProgram {
-	public void run(){		
-	GImage bender= new GImage("C:\\Users\\Miguel Gutierrez\\Documents\\Trabajo_Karel\\Imagenes\\bender.jpg");
-	double x=(bender.getWidth ());
-	double y=(bender.getHeight ());	
+public class imagenKarel extends GraphicsProgram{
+	public static final double ESCALA = 0.05;
+	private int x =10;
+	private int y =10;
+	public void run(){	
 	
-	add(bender,x ,y);
-	}		
-}
+	GImage bender= new GImage("../data/bender.jpg",x, y );
+	bender.scale(ESCALA);
+	add(bender);
+	move_x(bender);
+	move_y(bender);
+	move_x(bender);
+	move_x(bender);
 
+	}public void move_x(GImage bender) {
+		pause(300);
+		bender.setLocation(bender.getX()+20, bender.getY());
+		
+		
+	}
+	public void move_y(GImage bender) {
+		pause(300);
+		bender.setLocation(bender.getX(), bender.getY()+20 );
+	}
+
+		
+}	
 
